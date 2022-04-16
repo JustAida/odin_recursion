919196ee38664147d6f5337cf1ebb460b12210f2
JustAida <suradet40@gmail.com>
2022-04-17 00:23:24 +0700

    Change variable name

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -1,9 +1,9 @@
 def fibs(num)
-  num_array = []
+  fibs_array = []
   0.upto(num - 1) do |n|
-    num_array << (n <= 1 ? n : num_array[-2] + num_array[-1])
+    fibs_array << (n <= 1 ? n : fibs_array[-2] + fibs_array[-1])
   end
-  num_array
+  fibs_array
 end
 
 p fibs(8)

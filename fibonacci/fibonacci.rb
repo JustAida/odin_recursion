9ec68d90a94267e76c1aceb1c2e34f05e4d1fc83
JustAida <suradet40@gmail.com>
2022-04-22 07:53:06 +0700

    Add comments

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -1,5 +1,6 @@
 def fibs(num)
   fibs_array = []
+  # Use 0.upto(num) if the description of the assignment changed.
   0.upto(num - 1) do |n|
     fibs_array << (n < 2 ? n : fibs_array[-2] + fibs_array[-1])
   end
@@ -9,6 +10,7 @@ end
 p fibs(8)
 
 def fibs_rec(num, array = [])
+  # Use array << num if the description of the assignment changed.
   return array << 1 - num if num < 2
 
   array << fibs_rec(num - 1, array)[-1] + fibs_rec(num - 2)[-1]

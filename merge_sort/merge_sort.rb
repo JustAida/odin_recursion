74735878c73900a649adf4f721627abcf7cc228e
JustAida <suradet40@gmail.com>
2022-05-13 00:23:54 +0700

    Refactor the code

--- merge_sort/merge_sort.rb
+++ merge_sort/merge_sort.rb
@@ -1,30 +1,27 @@
 def merge_sort(array)
   return array if array.size < 2
 
-  array_size = array.size
-  left_half = merge_sort(array[0..array_size / 2 - 1])
-  right_half = merge_sort(array[array_size / 2..array_size - 1])
+  left_half = merge_sort(array[0..array.size / 2 - 1])
+  right_half = merge_sort(array[array.size / 2..])
 
   sorted_array = []
   left_index = 0
   right_index = 0
 
-  until sorted_array.size == array_size
-    if left_half[left_index].nil? 
-      sorted_array.concat(right_half[right_index..])
-      break
-    elsif right_half[right_index].nil?
-      sorted_array.concat(left_half[left_index..])
-      break
+  until left_index == left_half.size || right_index == right_half.size
+    if left_half[left_index] < right_half[right_index]
+      sorted_array.push(left_half[left_index])
+      left_index += 1
     else
-      if left_half[left_index] < right_half[right_index]
-        sorted_array.push(left_half[left_index])
-        left_index += 1
-      else
-        sorted_array.push(right_half[right_index])
-        right_index += 1
-      end
+      sorted_array.push(right_half[right_index])
+      right_index += 1
     end
+  end
+
+  if left_index > right_index
+    sorted_array.concat(right_half[right_index..])
+  else
+    sorted_array.concat(left_half[left_index..])
   end
   sorted_array
 end
